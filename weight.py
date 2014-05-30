d7f21d1d4e1b91337acf229bfc6bab01d801d202
Akanksha <akanksha.vision@gmail.com>
2014-05-30 14:03:37 +0200

    First version of weight.py to create weighted trees

---
+++ weight.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# Python modules
+import argparse
+import numpy
+#import xml.etree.ElementTree as ET
+# Homemade modules
+from particles import *
+# Argument parser setup
+parser = argparse.ArgumentParser()
+parser.add_argument("--infile", help="File in ROOT format to be read", nargs='?', type=str, default="test2.root")
+parser.add_argument("--intree", help="input TTree name", nargs='?', type=str, default="test")
+parser.add_argument("--infilew", help="File in ROOT format to be read (weights)", nargs='?', type=str, default="../BJetRegression/weights.root")
+parser.add_argument("--outfile", help="File in ROOT format for output", nargs='?', type=str, default="weighted_2_hbb.root")
+parser.add_argument("--outtree", help="output TTree name", nargs='?', type=str, default="test")
+
+args = parser.parse_args()
+# ROOT setup
+import ROOT
+from ROOT import gROOT
+from ROOT import TTree, TFile, TLorentzVector, TChain, TVector3
+# ROOT initialization
+gROOT.Reset()
+gROOT.SetBatch()
+chain = TChain(args.intree)
+chain.Add(args.infile)
+
+weightFile = TFile.Open(args.infilew)
+w_ggHH0_hbb_pt = weightFile.Get("hFrac_ggHH2_8TeV_gr_hbb_p4_pt")
+#ibin = w_ggHH0_hbb_pt.FindBin(200)
+#print w_ggHH0_hbb_pt.GetBinContent(ibin)
+
+
+########################################################################################
+# WRITE THE ROOT FILE
+########################################################################################
+outfile = TFile(args.outfile, "recreate")
+outtree = TTree(args.outtree, args.outtree)
+
+hbb_pt = numpy.zeros(1, dtype=float)
+hbb_new_pt = numpy.zeros(1, dtype=float)
+
+outtree.Branch('gr_hbb_w_pt', hbb_pt, 'gr_hbb_w_pt/D')
+outtree.Branch('gr_hbb_new_w_pt', hbb_new_pt, 'gr_hbb_new_w_pt/D')
+
+gr_hbb_p4_pt = numpy.zeros(1, dtype=float)
+
+print chain.GetEntries()
+for ievt in xrange(chain.GetEntries()):
+    if ievt > 100:
+        break
+    chain.GetEntry(ievt)
+    b1 = TLorentzVector()
+    b2 = TLorentzVector()
+    hbb_new = TLorentzVector()
+    hbb = TLorentzVector()
+    b1.SetPtEtaPhiM(chain.gr_b1_p4_pt, chain.gr_b1_p4_eta, chain.gr_b1_p4_phi, chain.gr_b1_p4_mass)
+    b2.SetPtEtaPhiM(chain.gr_b2_p4_pt, chain.gr_b2_p4_eta, chain.gr_b2_p4_phi, chain.gr_b2_p4_mass)
+    hbb.SetPtEtaPhiM(chain.gr_hbb_p4_pt, chain.gr_hbb_p4_eta, chain.gr_hbb_p4_phi, chain.gr_hbb_p4_mass)
+    
+#    b1.Boost(10*b1.Px(), 10*b1.Py(), b1.Pz())
+#    b1.Boost(0.5, 0.5, 0)
+#    print b1.Px()
+#    b2.Boost(0.5, 0.5, 0)
+#    print b2.Px()
+
+    w_ggHH0_hbb_pt = weightFile.Get("hFrac_ggHH2_8TeV_gr_hbb_p4_pt")
+    ibin = w_ggHH0_hbb_pt.FindBin(chain.gr_hbb_p4_pt)
+    w = w_ggHH0_hbb_pt.GetBinContent(ibin)
+#    w = h1D_pt_data -> GetBinContent(h1D_pt_data -> FindBin(gr_hbb_p4_pt)
+#    print 
+
+    spatial_b1 = TVector3(b1.Px()*w, b1.Py()*w, b1.Pz())
+#    b1_new = b1.SetVectM(spatial_b1, b1.M())
+    b1.SetVectM(spatial_b1, b1.M())
+#    print b1.Px()
+    spatial_b2 = TVector3(b2.Px()*w, b2.Py()*w, b2.Pz())
+    b2.SetVectM(spatial_b2, b2.M())
+
+    spatial_h = TVector3(hbb.Px()*w, hbb.Py()*w, hbb.Pz())
+    hbb.SetVectM(spatial_h, hbb.M())
+ 
+    hbb_new = b1 + b2 
+
+    hbb_pt[0] = hbb.Pt()
+    hbb_new_pt[0] = hbb_new.Pt()
+    if ievt % 1000 == 0:
+        print hbb_pt[0] - hbb_new_pt[0], hbb_pt[0], hbb_new_pt[0], w, gr_hbb_p4_pt, ibin
+    outtree.Fill()
+   
+# write the tree into the output file and close the file
+outfile.Write()
+outfile.Close()
+
